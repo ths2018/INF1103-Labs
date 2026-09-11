77bde75ce4e76dad26fb481385a323d2f0cd4ca3
Han Sheng <2603667@sit.singaporetech.edu.sg>
2026-09-11 20:40:53 +0800

    Part 7 and completed

--- auditor.py
+++ auditor.py
@@ -7,14 +7,20 @@ print("Welcome to the Inventory Auditor!")
 print("Please enter the number of stock quantity:")
 
 while quit != "quit":
-    newstock = input()
-    if newstock != "quit" and newstock.isdigit():
-        newstock = int(newstock)
+    userinput = input()
+    if userinput == "quit":
+        print("Exiting the Inventory Auditor. Total unit processed:", totalstock, "and Number of rejected entries:", rejectcounter)
+        break
+    elif userinput.isdigit():
+        newstock = int(userinput)
         totalstock += newstock
-        print("Total stock entries entered:", totalstock, "Please enter the next stock quantity or type 'quit' to exit.")
-    elif newstock != "quit" and not newstock.isdigit():
+        inventory += newstock
+        if inventory > 500:
+            print("Inventory limit exceeded. exiting the program.")
+            break
+        else:
+            print("Total stock entries entered:", totalstock, "Please enter the next stock quantity or type 'quit' to exit.")
+    else:
         rejectcounter += 1
         print("Invalid input. Please enter a valid number or type 'quit' to exit.")
-    elif newstock == "quit":
-        print("Exiting the Inventory Auditor. Total unit processed:", inventory+totalstock, "and Number of rejected entries:", rejectcounter)
-        break
+    
