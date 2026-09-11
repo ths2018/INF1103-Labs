7728965334e59a1136538396da90a8ee1b88609b
Han Sheng <2603667@sit.singaporetech.edu.sg>
2026-09-11 15:27:31 +0800

    Part 2

--- auditor.py
+++ auditor.py
@@ -1,2 +1,10 @@
 inventory = int(0)
+totalstock = int(0)
+quit = ""
 
+while quit != "quit":
+    print("Welcome to the Inventory Auditor!")
+    print("Please enter the number of stock quantity:")
+    newstock = input()
+    if newstock == "quit":
+        break
