1236f8cde24849cbf5bcbd7fb76e3e962fc42c35
Han Sheng <2603667@sit.singaporetech.edu.sg>
2026-09-22 11:50:34 +0800

    process_delivery(current_total, new_value)

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,5 +1,4 @@
 inventory = int(0)
-totalstock = int(0)
 rejectcounter = int(0)
 
 def get_valid_input(user):
@@ -10,19 +9,22 @@ def get_valid_input(user):
     else:
         return False
 
+def process_delivery(current_stock, new_stock):
+    current_stock += new_stock
+    return current_stock
+
 print("Welcome to the Inventory Auditor!")
 print("Please enter the number of stock quantity:")
 
 while True:
     userinput = input()
     if get_valid_input(userinput) is None:
-        print("Exiting the Inventory Auditor. Total unit processed:", totalstock, "and Number of rejected entries:", rejectcounter)
+        print("Exiting the Inventory Auditor. Total unit processed:", inventory, "and Number of rejected entries:", rejectcounter)
         break
     elif get_valid_input(userinput) is True:
         newstock = int(userinput)
-        totalstock += newstock
-        inventory += newstock
-        print("Total stock entries entered:", totalstock, "Please enter the next stock quantity or type 'quit' to exit.")
+        inventory = process_delivery(inventory, newstock)
+        print("Total stock entries entered:", inventory, "Please enter the next stock quantity or type 'quit' to exit.")
     elif get_valid_input(userinput) is False:
         rejectcounter += 1
         print("Invalid input. Please enter a valid number or type 'quit' to exit.")
